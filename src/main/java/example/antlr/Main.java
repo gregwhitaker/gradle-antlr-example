9fae7924544a3ba12bcfb4c647cbcb8fdf2d6549
gregwhitaker <gwhitake@gmail.com>
2019-12-14 16:44:06 -0800

    Initial Commit

---
+++ src/main/java/example/antlr/Main.java
@@ -0,0 +1,12 @@
+package example.antlr;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger LOG = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String... args) {
+        LOG.info("Running the example...");
+    }
+}
